42db6650607e9840cf5a29234eeb4842bf292dc7
liang zhen <zhen.liang@rea-group.com>
2021-01-18 20:05:29 +0800

    Update run-test.sh for jenkins execution.

--- buildtasks/run-test-docker.sh
+++ buildtasks/run-test-docker.sh
@@ -5,7 +5,8 @@ echo " *** Start testing in docker *** "
 
 STATUS_CODE=$?
 
-docker run -it -w /test -v `dirname "$PWD"`:/root/.gradle -v `pwd`:/test nova:nova-web-test bash ./run-test.sh
+docker run -w /test -v `dirname "$PWD"`:/root/.gradle -v `pwd`:/test nova:nova-web-test bash ./run-test.sh
+#docker run -it -w /test -v `dirname "$PWD"`:/root/.gradle -v `pwd`:/test nova:nova-web-test bash ./run-test.sh
 #docker run -it -w /test -v `dirname "$PWD"`:/root/.gradle -v `pwd`:/test phoebusliang:e2e-java bash ./run-test.sh
 #docker run -it -w /test -v `dirname "$PWD"`:/root/.gradle -v `pwd`:/test lz:lz-test bash ./run-test.sh
 
